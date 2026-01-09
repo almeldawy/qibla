5125cfc409dec1dd8b2a245a2b024b45c7c02401
almeldawy <almeldawy@gmail.com>
2026-01-09 09:49:12 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,38 @@
-const CACHE_NAME = 'qibla-cache-v1';
+const CACHE_NAME = 'qibla-v2';
 const ASSETS = [
-  'index.html',
-  'manifest.json'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
-// Installation : on met les fichiers dans le "frigo" (cache)
+// Installation : on met les fichiers essentiels en cache
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(ASSETS))
+      .then((cache) => {
+        // On essaie d'ajouter les ressources, mais on ne bloque pas si une échoue
+        return cache.addAll(ASSETS);
+      })
       .then(() => self.skipWaiting())
   );
 });
 
-// Activation : on nettoie les vieux caches
+// Activation : on vide l'ancien cache si on change de version
 self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
 });
 
-// Stratégie : Répondre avec le cache, sinon chercher sur le réseau
+// Stratégie : Réseau d'abord, sinon cache (pour être sûr d'avoir la dernière version)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
   );
 });
